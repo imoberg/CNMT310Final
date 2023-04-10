db025bb44bf35f334297f27257eb90a1cc30d205
Moberg <imobe456@uwsp.edu>
2023-04-10 12:24:50 -0500

    Creating the bookmarks php and css page and making updates so that each page was created with the Page class

---
+++ form-login.php
@@ -0,0 +1,37 @@
+<?php
+require_once(__DIR__ . "/classes/Page/Page.class.php");
+$login  = new Page("login");
+
+$login->addHeadElement("<link href='./css/login-styles.css' rel='stylesheet'>");
+$login->addHeadElement("<title>Login Page</title>");
+print $login->getTopSection();
+print '        <div class="box">';
+print '            <div class="navBar"></div>';
+print '            <div class="logo"></div>';
+print '            <a href="index.php" class="bookmarks">Bookmarks |</a>';
+print '            <a href="index.php" class="navHome" >Home  |</a>';
+print '            <a href="form-login.php" class="navLogin">Login  |</a>';
+print '            <span class="welcomeTxt">Welcome Back, Please Login To Your Account.</span>';
+//print '            <form action="action-login.php" method="POST">'; this is what we are actually using
+print '            <form action="bookmarks.php" method="POST">'; //this is testing
+print '                <span class="username">Username</span>';
+print '                <input type="text" id="username" name="username" class="inputUsername">';
+print '                <span class="password">Password</span>';
+print '                <input type="text" id="password" name="password" class="inputPassword">';
+print '                <input type="submit" value="Login" class="submit">';
+print '            </form>';
+print '            <div class="workBack"></div>';
+print '            <span class="workHead">How It Works?</span>';
+print '            <ul class="workTxt">';
+print '                <li>Houses All Your Bookmarks</li><br>';
+print '                <li>Sorts Them By Popularity</li><br>';
+print '                <li>Can Add Bookmarks</li><br>';
+print '                <li>Can Delete Bookmarks</li><br>';
+print '                <li>Can Share Bookmarks</li><br>';
+print '            </ul>';
+$login->addBottomElement("<div class='footerBack'></div>");
+$login->addBottomElement("<footer class='footerTxt'>&copy Copyright Isaac Moberg UWSP 2023</footer>;");
+print $login->getBottomSection();
+
+
+?>
